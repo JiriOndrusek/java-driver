561f3af673c60a01073b8db2e6415bef1351df86
olim7t <omichallat+github@gmail.com>
2020-08-10 12:08:49 -0700

    [fixup] complete unit test

--- integration-tests/src/test/java/com/datastax/oss/driver/mapper/ImmutableIT.java
+++ integration-tests/src/test/java/com/datastax/oss/driver/mapper/ImmutableIT.java
@@ -16,15 +16,70 @@
 package com.datastax.oss.driver.mapper;
 
 import static com.datastax.oss.driver.api.mapper.entity.naming.GetterStyle.SHORT;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.mapper.MapperBuilder;
+import com.datastax.oss.driver.api.mapper.annotations.CqlName;
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.DaoFactory;
+import com.datastax.oss.driver.api.mapper.annotations.DaoKeyspace;
 import com.datastax.oss.driver.api.mapper.annotations.Entity;
+import com.datastax.oss.driver.api.mapper.annotations.Insert;
 import com.datastax.oss.driver.api.mapper.annotations.IntrospectionStrategy;
+import com.datastax.oss.driver.api.mapper.annotations.Mapper;
 import com.datastax.oss.driver.api.mapper.annotations.PartitionKey;
+import com.datastax.oss.driver.api.mapper.annotations.Select;
+import com.datastax.oss.driver.api.testinfra.ccm.CcmRule;
+import com.datastax.oss.driver.api.testinfra.session.SessionRule;
+import com.datastax.oss.driver.categories.ParallelizableTests;
+import java.util.Objects;
 import java.util.UUID;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
 
+@Category(ParallelizableTests.class)
 public class ImmutableIT extends InventoryITBase {
 
+  private static final CcmRule CCM_RULE = CcmRule.getInstance();
+  private static final SessionRule<CqlSession> SESSION_RULE = SessionRule.builder(CCM_RULE).build();
+
+  @ClassRule
+  public static final TestRule CHAIN = RuleChain.outerRule(CCM_RULE).around(SESSION_RULE);
+
+  private static ImmutableProductDao dao;
+
+  @BeforeClass
+  public static void setup() {
+    CqlSession session = SESSION_RULE.session();
+
+    for (String query : createStatements(CCM_RULE)) {
+      session.execute(
+          SimpleStatement.builder(query).setExecutionProfile(SESSION_RULE.slowProfile()).build());
+    }
+
+    InventoryMapper mapper = InventoryMapper.builder(session).build();
+    dao = mapper.immutableProductDao(SESSION_RULE.keyspace());
+  }
+
+  @Test
+  public void should_insert_and_retrieve_immutable_entities() {
+    ImmutableProduct originalProduct =
+        new ImmutableProduct(UUID.randomUUID(), "mock description", new Dimensions(1, 2, 3));
+    dao.save(originalProduct);
+
+    ImmutableProduct retrievedProduct = dao.findById(originalProduct.id());
+    assertThat(retrievedProduct).isEqualTo(originalProduct);
+  }
+
   @Entity
+  @CqlName("product")
   @IntrospectionStrategy(getterStyle = SHORT, mutable = false)
   public static class ImmutableProduct {
     @PartitionKey private final UUID id;
@@ -48,5 +103,43 @@ public class ImmutableIT extends InventoryITBase {
     public Dimensions dimensions() {
       return dimensions;
     }
+
+    @Override
+    public boolean equals(Object other) {
+      if (other == this) {
+        return true;
+      } else if (other instanceof ImmutableProduct) {
+        ImmutableProduct that = (ImmutableProduct) other;
+        return Objects.equals(this.id, that.id)
+            && Objects.equals(this.description, that.description)
+            && Objects.equals(this.dimensions, that.dimensions);
+      } else {
+        return false;
+      }
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(id, description, dimensions);
+    }
+  }
+
+  @Mapper
+  public interface InventoryMapper {
+    static MapperBuilder<InventoryMapper> builder(CqlSession session) {
+      return new ImmutableIT_InventoryMapperBuilder(session);
+    }
+
+    @DaoFactory
+    ImmutableProductDao immutableProductDao(@DaoKeyspace CqlIdentifier keyspace);
+  }
+
+  @Dao
+  public interface ImmutableProductDao {
+    @Select
+    ImmutableProduct findById(UUID productId);
+
+    @Insert
+    void save(ImmutableProduct product);
   }
 }
